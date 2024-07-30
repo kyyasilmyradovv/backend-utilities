07a4c61bc8208025acf4d0a256e5acdcadcf9e88
Kyyas Ilmyradov <78571757+kyyasilmyradovv@users.noreply.github.com>
2024-07-31 02:33:34 +0500

    Update deleteFile function

--- deleteFile.js
+++ deleteFile.js
@@ -1,9 +1,13 @@
+// This function will delete a file with provided filename(destination)
 const fs = require('fs').promises;
 
 exports.deleteFile = async (filename) => {
   if (filename?.length) {
     try {
+      // Check if file exists
       await fs.access(filename, fs.constants.F_OK);
+
+      // Delete if file exists
       fs.unlink(filename);
     } catch (err) {}
   }
